eb2b0b8e2ba05e658cf46e4c059de859de62fcf7
temple69 <utemple169@gmail.com>
2023-06-01 14:44:36 +0100

    update:defined an interface for the children parameters

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Navbar from '../Navbar'
-Navbar
-
-const Layout = (props:any) => {
-  return (
- <>
-    <Navbar/>
-    <main>
-        {props.children}
-    </main>
-    
-    
-    </>
-  )
+import React, { FC, ReactNode } from "react";
+import Navbar from "../Navbar";
+interface ParentComponentProps {
+  children: ReactNode;
 }
+//General Layout for All pages
+const Layout = ({ children }: ParentComponentProps) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
