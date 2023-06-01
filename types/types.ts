42e1cba02e55b14b1422a128fb93935956ee2601
temple69 <utemple169@gmail.com>
2023-06-01 15:55:18 +0100

    folder which contains types used in this project

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,4 @@
+ //declaring types for the rendered news article
  type newsDataType = {
     id: string;
     title: string;
@@ -5,8 +6,9 @@
     author: string;
     publishedDate: number;
     imgUrl: string;
-    authorFallBack?: string;
+    content:string
   };
+  //Declaring types for the initial Redux State
   export type NewsState = {
     newsList: newsDataType[];
     loading: boolean;
@@ -18,12 +20,19 @@
       description: string;
     }[];
   };
+  // Declaring types for the newsList Component
   export type newsListType = {
     newsList: newsDataType;
-    isLoading?:boolean
+    isLoading?:boolean,
+    isSinglePage?:boolean
   };
+  //Declaring types for the pagination component
   export type PaginationType={
     pageCount:number
     handlePageClick:(page: {selected: number;})=>void,
     isArrayEmpty:boolean
 }
+//Declaring types for the news component
+export type newsType = {
+  isNotNews: boolean;
+};
