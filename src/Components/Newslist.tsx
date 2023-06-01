e9e44643011ef2b70f6bfd6489cbdc9c0eddbb85
temple69 <utemple169@gmail.com>
2023-06-01 17:20:33 +0100

    styles:updated some elements and made component responsive

--- src/Components/Newslist.tsx
+++ src/Components/Newslist.tsx
@@ -1,34 +1,65 @@
-import React, { FC } from "react";
+import React from "react";
 import Link from "next/link";
 import { newsListType } from "../../types/types";
 
-
-const Newslist = ({ newsList,isLoading,isSinglePage }: newsListType) => {
+const Newslist = ({ newsList, isLoading, isSinglePage }: newsListType) => {
   //Destructuring the props that will be receuved from the parents
-  const {id,imgUrl,title,description,author,publishedDate,content} =newsList
+  const { id, imgUrl, title, description, author, publishedDate, content } =
+    newsList;
   return (
-    <figure className={isSinglePage?'bg-red-500 customShadow flex flex-col w-[80%] m-auto mt-5':'customShadow aspect-auto'} key={id}>
-      <h1 className="font-bold text-center px-2 py-4">{title?title:''}</h1>
-      {!isLoading?<img src={imgUrl ? imgUrl! :"/defaultImage.jpg"} alt={title} className="w-full  object-cover" />:''}
-      {!isLoading?<figcaption className="px-2 py-2 relative">
-        <p>{description?description:''}</p>
+    /* Depending on the isSinglePage props some elements are hidden and some css classes are dynamically changed */
+    <figure
+      className={
+        isSinglePage
+          ? "bg-red-500 customShadow flex flex-col w-[80%] m-auto mt-5"
+          : "customShadow aspect-auto"
+      }
+      key={id}
+    >
+      <h1 className="font-bold text-center px-2 py-4">{title ? title : ""}</h1>
+      {!isLoading ? (
+        <img
+          src={imgUrl ? imgUrl! : "/defaultImage.jpg"}
+          alt={title}
+          className="w-full  object-cover"
+        />
+      ) : (
+        ""
+      )}
+      {!isLoading ? (
+        <figcaption className="px-2 py-2 relative">
+          <p>{description ? description : ""}</p>
 
-        <small className="text-[18px] flex gap-1">
-          <span className="font-bold block">Author:</span>
-          {author?author:''}
-        </small>
-        <h5>
-          {" "}
-          <span>Published:</span> {publishedDate?publishedDate<=0?'Today': `${publishedDate} days ago`:''}
-        </h5>
-        {isSinglePage?<p>{content?content:''}</p>:''}
-        {!isSinglePage?isLoading?'':<button className="bg-green-500 w-[90%]  text-white mx-2 h-10 rounded-md bottom-0 top-3">
-        <Link passHref href={`Home/${id}`}>
-          See More Details
-        </Link>{" "}
-      </button>:''}
-      </figcaption>:''}
-     
+          <p className="text-[14px] sm:text-[12px] flex gap-1 font-bold ">
+            {author ? author : ""}
+          </p>
+          <h5 className="my-2">
+            {" "}
+            <span>Published:</span>{" "}
+            {publishedDate
+              ? publishedDate <= 0
+                ? "Today"
+                : `${publishedDate} days ago`
+              : ""}
+          </h5>
+          {isSinglePage ? <p>{content ? content : ""}</p> : ""}
+          {!isSinglePage ? (
+            isLoading ? (
+              ""
+            ) : (
+              <button className="bg-green-500 w-[90%]  text-white mx-2 my-2 h-10 rounded-md bottom-0 top-3">
+                <Link passHref href={`Home/${id}`}>
+                  See More Details
+                </Link>{" "}
+              </button>
+            )
+          ) : (
+            ""
+          )}
+        </figcaption>
+      ) : (
+        ""
+      )}
     </figure>
   );
 };
