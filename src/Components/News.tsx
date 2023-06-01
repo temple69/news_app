051a684ec0733f8f45d29c779b250ccab203f44f
temple69 <utemple169@gmail.com>
2023-06-01 14:37:21 +0100

    reusable component that fetches and renders data based on route paramters

---
+++ src/Components/News.tsx
@@ -0,0 +1,154 @@
+//Redux Toolkit Hooks
+import { useAppDispatch, useAppSelector } from "@/app/hooks/hooks";
+Poppins
+
+//React Events
+import React, {
+  FormEvent,
+  useEffect,
+  useRef,
+  useState,
+  ChangeEvent,
+} from "react";
+//Function from  redux store
+import {
+  newsData,
+  getAllNews,
+  getAllNewsSources,
+} from "@/features/News/newsSlice";
+//React Components
+import Newslist from "./Newlist";
+import Pagination from "./Pagination";
+import Modal from "./Modal/Modal";
+import { useRouter } from "next/router";
+//Constants
+const PER_PAGE = 10;
+//User-defined types
+import { newsType } from "../../types/types";
+import { Poppins } from "next/font/google";
+
+const News = ({ isNotNews }: newsType) => {
+  //State management from redux toolkit
+  const dispatch = useAppDispatch();
+  const { loading, newsList, newsSources, errorMessage, error } =
+    useAppSelector(newsData);
+  const searchRef = useRef<HTMLInputElement>(null);
+  //getting article id from route parameters  to impement dynamic fetching
+  const router = useRouter();
+  const articleId = router.query.articlesTopic! as string;
+
+  //Custom state Managements
+  const [currentPage, setCurrentPage] = useState(0);
+  const [newsSource, setNewsSource] = useState<string>("");
+  // This functions get the value of the select html element
+  const filterNewsBySourceHandler = (event: ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    setNewsSource(value);
+  };
+  /* if article id is defined This hook Fetches data using redux toolkit state management functions when the component mounts and refetches if there is a change in html select element  if undefined uses fetches data dynamically*/
+  useEffect(() => {
+    let articletopic = "worldnews";
+    dispatch(getAllNewsSources(newsSource));
+    if (articleId === undefined && newsSource === "") {
+      dispatch(getAllNews(articletopic));
+    } else {
+      articleId;
+      dispatch(getAllNews(articleId));
+    }
+  }, [newsSource, articleId]);
+  /* This function is used by the react-paginate component to know the current page number index that was clicked by user */
+  const handlePageClick = (page: { selected: number }) => {
+    setCurrentPage(page.selected);
+  };
+  //Operations to determine the number of news articles per page
+  const offset = currentPage * PER_PAGE;
+  const pageCount = Math.ceil(newsList.length / PER_PAGE);
+  const CurrentPageData = newsList.slice(offset, offset + PER_PAGE);
+  /* Function that executes when the form is submiited  if the value of input is undefined alerts user to type in a value else it fetches data with the value */
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+    let title = searchRef.current?.value as string;
+    if (title) {
+      title = title.toLowerCase();
+      title = title.trim();
+      dispatch(getAllNews(title));
+    } else {
+      alert("Please input topic to search");
+    }
+    searchRef.current!.value = "";
+  };
+
+  return (
+    /*Css Classes are updated dynamically based on the `isNotNews` state  */
+    <section className="px-10 pt-4">
+      {/* Renders the modal component depending on the state */}
+      {loading ? <Modal /> : ""}
+      <form
+        className={isNotNews ? "grid grid-cols-1" : `grid grid-cols-2 gap-10`}
+        onSubmit={submitHandler}
+      >
+        {isNotNews ? (
+          ""
+        ) : (
+          <label htmlFor="">
+            Which Topic Would You like to Read About Today
+            <input
+              type="text"
+              className="border block w-[80%] h-10 rounded-lg px-2"
+              ref={searchRef}
+            />
+            <button className="h-10 border bg-green-600 text-white rounded-lg w-[80%] my-3">
+              Get-News-Article
+            </button>
+          </label>
+        )}
+        <label htmlFor="">
+          Filter By News Source:
+          <select
+            name=""
+            id=""
+            className={
+              isNotNews
+                ? "border w-full mb-6 block h-10 rounded-lg"
+                : "border w-[80%] my-3 block h-10 rounded-lg px -2"
+            }
+            onChange={filterNewsBySourceHandler}
+          >
+            <option value=""></option>
+            {newsSources.map((source) => (
+              <option
+                value={source.id}
+                title={source.description}
+                key={source.id}
+              >
+                {source.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+
+      <div className="responsive_grid mb-4">
+        {CurrentPageData.length === 0 ? (
+          <p className="text-center text=[20px] text-red">{`${errorMessage}`}</p>
+        ) : (
+          CurrentPageData.map((article) => (
+            <Newslist newsList={article} isLoading={loading} />
+          ))
+        )}
+      </div>
+
+      {loading ? (
+        ""
+      ) : (
+        <Pagination
+          pageCount={pageCount}
+          handlePageClick={handlePageClick}
+          isArrayEmpty={error}
+        />
+      )}
+    </section>
+  );
+};
+
+export default News;
