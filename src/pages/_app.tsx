6b58f70bbe44fe37f035f89bc7611e221ac2b7c3
temple69 <utemple169@gmail.com>
2023-06-01 15:50:17 +0100

    main root file where other components are rendered

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
+import { store } from "@/app/store/store";
+import Layout from "@/Components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </Layout>
+  );
 }
